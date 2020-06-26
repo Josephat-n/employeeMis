2a6c8f9ddcd0a60db3540c06e023975038a3a1e9
Josephat-n <josphatnjoroge254@gmail.com>
2020-06-26 23:30:18 +0300

    update the index page to display data from the database.

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,14 @@
 from flask import render_template
 from . import main
+from .. import db
+from ..models import Blog
 
 
 @main.route('/')
 def index():
-   return render_template('index.html')
+   """
+   Fetch the employees and details from the bd.
+   """
+   employees = Employee.query.all()
+   print(employees)
+   return render_template('index.html', employees = employees)
